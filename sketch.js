b197cf2193ff4ba3dc05ec0409df6ca00e8dccb6
SCguerra <sguerr5@lsu.edu>
2019-09-01 15:04:28 -0500

    this is done now

--- sketch.js
+++ sketch.js
@@ -1,8 +1,17 @@
 function setup(){
-    createCanvas (800,800);
-    background (51);
-    ellipse(30, 30, 50, 50);
-    
+    createCanvas (900,900);
+    background (0)
+    ellipse(600, 600, 300, 300);
+    ellipse(300, 350, 200, 200);
+    ellipse(200, 300, 100, 100);
+    ellipse(300, 200, 60, 60);
+    ellipse(650, 475, 50, 50);
+    ellipse(700, 150, 40, 40);
+    ellipse(600, 125, 30, 30);
+    ellipse(350, 550, 20, 20);
+    ellipse(370, 565, 15, 15);
+    ellipse(275, 500, 10, 10);
+
 }
 function draw(){
     
